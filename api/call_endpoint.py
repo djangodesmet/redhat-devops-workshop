7c6857c989570d4ccc60ab6e33599e5570cbf4f1
Django de Smet <djangodesmet@live.nl>
2024-11-13 10:16:26 +0000

    works locally now

--- api/call_endpoint.py
+++ api/call_endpoint.py
@@ -4,7 +4,6 @@ import logging
 import os
 
 import requests
-from aapy import AAPInvoker # TODO: do not use aapy package
 from urllib3 import disable_warnings
 from urllib3.exceptions import InsecureRequestWarning
 
@@ -26,23 +25,10 @@ def main(local: bool = False):
         # lokaal volstaat het om met requests een post te doen omdat we hier geen token
         # op moeten halen om een call naar de API te kunnen doen.
         response = requests.post(
-            verify=False, url=f"http://127.0.0.1:9000/{endpoint}", data=json.dumps(data)
+            verify=False, url=f"http://127.0.0.1:8000/{endpoint}", data=json.dumps(data)
         ).json()
     else:
-        if (namespace := os.getenv("NAMESPACE", None)) is None:
-            raise RuntimeError(
-                "You are trying to call an inference API without specifying namespace."
-            )
-        # remote gebruiken we de AAPInvoker class om een call te doen naar de API
-        # in een inference omdat we hier een token voor nodig hebben. Dit gebeurt in
-        # onder water in het AAPInvoker object.
-        invoker = AAPInvoker(
-            name_of_api=os.getenv("API_APP_NAME"),
-            namespace=namespace,
-            platform=os.getenv("PLATFORM", "AAP"),
-        )
-        response = invoker.post_request(data, endpoint)
-    logging.info(f"Response from /{endpoint} endpoint: {response}")
+        raise NotImplementedError
 
 
 if __name__ == "__main__":
